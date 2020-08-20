ddfda1cd3694852d650c2b503b6d309110b9490e
sparkki <satuparkkinen@gmail.com>
2020-08-20 18:48:28 +0300

    Updates

--- precache-manifest.e9485696bd339966f5bcba12c2da7cb1.js
+++ precache-manifest.e9485696bd339966f5bcba12c2da7cb1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a2139607539795f3163c3450118bb8f5",
+    "revision": "2590477fdadaabc8673aec5502a6da4b",
     "url": "/book-list/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/book-list/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "2b249f018aa0c78c5b8f",
+    "revision": "d1e2386c797902d32366",
     "url": "/book-list/static/css/main.7400b930.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/book-list/static/js/2.f0b97aa3.chunk.js.LICENSE"
   },
   {
-    "revision": "2b249f018aa0c78c5b8f",
-    "url": "/book-list/static/js/main.65d5c9f9.chunk.js"
+    "revision": "d1e2386c797902d32366",
+    "url": "/book-list/static/js/main.1867707b.chunk.js"
   },
   {
     "revision": "33e82236228384561cf8",
