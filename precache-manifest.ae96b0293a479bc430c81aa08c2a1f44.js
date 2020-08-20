95ac9667ac8dfda189320d26732e26436bbc7a4a
sparkki <satuparkkinen@gmail.com>
2020-08-20 12:09:53 +0300

    Updates

---
+++ precache-manifest.ae96b0293a479bc430c81aa08c2a1f44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33046a16711357122ae30fcc4e508f90",
+    "url": "/book-list/index.html"
+  },
+  {
+    "revision": "9b0ac8ef1afa7ea74355",
+    "url": "/book-list/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1adcf6540e24765d59f6",
+    "url": "/book-list/static/css/main.a42e3b9d.chunk.css"
+  },
+  {
+    "revision": "9b0ac8ef1afa7ea74355",
+    "url": "/book-list/static/js/2.f0b97aa3.chunk.js"
+  },
+  {
+    "revision": "250c8e2a12f8390bbdd5eeb22983d0f1",
+    "url": "/book-list/static/js/2.f0b97aa3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1adcf6540e24765d59f6",
+    "url": "/book-list/static/js/main.c81e04d4.chunk.js"
+  },
+  {
+    "revision": "33e82236228384561cf8",
+    "url": "/book-list/static/js/runtime-main.4020af2f.js"
+  }
+]);
