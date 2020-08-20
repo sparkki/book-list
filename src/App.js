c71ae76dc3e05b98d3f62d1449516ce720a6c8d9
sparkki <satuparkkinen@gmail.com>
2020-08-20 15:22:58 +0300

    editorial

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import BookList from './components/BookList';
 import './styles/App.scss';
 //const apiEndpoint = 'http://localhost:5000/books';
 //const apiEndpoint = 'https://my-json-server.typicode.com/books/';
-const apiEndpoint = 'http://sparkki.github.io/book-list/db.json';
+const apiEndpoint = 'https://sparkki.github.io/book-list/db.json';
 
 
 class App extends React.Component {
